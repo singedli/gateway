11dc98ca08c4ab2858b9c32e045004d9bf61760c
singedli <634814660@qq.com>
2019-11-22 10:40:35 +0800

    添加模板代码

--- src/main/java/com/ocft/gateway/handler/AbstractControllerHandler.java
+++ src/main/java/com/ocft/gateway/handler/AbstractControllerHandler.java
@@ -9,11 +9,48 @@ import java.util.Map;
  * @Title: AbstractControllerHandler
  * @ProjectName gateway
  * @date 2019/11/21下午6:18
- * @Description:
+ * @Description: 抽象处理器控制器类
  */
-public class AbstractControllerHandler implements IControllerHandler {
+public abstract class AbstractControllerHandler implements IControllerHandler {
+
+    /**
+     * 构造请求报文头
+     *
+     * @return
+     */
+    public abstract String buildReqHeader();
+
+    /**
+     * 构造请求报文体
+     *
+     * @param body
+     * @return
+     */
+    public abstract String buildReqBody(String body);
+
+    /**
+     * 发送报文给后台
+     *
+     * @param header
+     * @param body
+     * @return
+     */
+    public abstract String sendToBacon(String header, String body);
+
+    /**
+     * 返回前端处理
+     *
+     * @param resout
+     * @return
+     */
+    public abstract Map<String, Object> retToClient(String resout, HttpServletRequest request);
+
+
     @Override
     public Map<String, Object> handle(String body, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        String requestHeader = this.buildReqHeader();
+        String requestBody = this.buildReqBody(body);
+        String responseString = sendToBacon(requestHeader, requestBody);
+        return retToClient(responseString, request);
     }
 }
--- src/main/java/com/ocft/gateway/handler/ConcurrentControllerHandler.java
+++ src/main/java/com/ocft/gateway/handler/ConcurrentControllerHandler.java
@@ -1,5 +1,8 @@
 package com.ocft.gateway.handler;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 /**
  * @author lijiaxing
  * @Title: ConcurrentControllerHandler
@@ -8,4 +11,23 @@ package com.ocft.gateway.handler;
  * @Description: 并发类型的处理器
  */
 public class ConcurrentControllerHandler extends AbstractControllerHandler {
+    @Override
+    public String buildReqHeader() {
+        return null;
+    }
+
+    @Override
+    public String buildReqBody(String body) {
+        return null;
+    }
+
+    @Override
+    public String sendToBacon(String header, String body) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> retToClient(String resout, HttpServletRequest request) {
+        return null;
+    }
 }
