d2b81cc28afdb11149d7fec241dfb4c1451fd25b
singedli <634814660@qq.com>
2019-11-27 16:22:23 +0800

    添加json剪裁功能

--- src/main/java/com/ocft/gateway/common/evaluator/JsonSlimEvalutor.java
+++ src/main/java/com/ocft/gateway/common/evaluator/JsonSlimEvalutor.java
@@ -1,7 +1,14 @@
 package com.ocft.gateway.common.evaluator;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ocft.gateway.common.parser.JsonSlimParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * @author lijiaxing
@@ -10,7 +17,106 @@ import org.springframework.stereotype.Service;
  * @date 2019/11/23下午5:05
  * @Description: json 剪裁处理程序
  */
-@Slf4j
-@Service
 public class JsonSlimEvalutor {
+    private static final String INNER_SPLIT_FLAG = ",";
+
+    public static <T extends JSON> T remove(T t, String paths){
+        String[] split = paths.split(INNER_SPLIT_FLAG);
+        for (String path : split) {
+            deleteJsonProperty(t, path);
+        }
+        return t;
+    }
+
+    public static <T extends JSON> T retain(T t, String paths){
+        Map<String, Object> pathMap = JsonSlimParser.pathStr2Map(paths);
+        TreeSet<String> resultSet = new TreeSet<>();
+        JsonSlimParser.getKeepStylePath("",pathMap,resultSet);
+        keepKeysByGivenKeySet(resultSet,t);
+        return t;
+    }
+
+    /**
+     * 递归删除指定字段
+     *
+     * @param outterObject
+     * @param source
+     * @return
+     */
+    private static void deleteJsonProperty(Object outterObject, String source) {
+        if (outterObject == null || source == null || "".equals(source))
+            return;
+        if (outterObject instanceof JSONObject) {
+            JSONObject tempJson = (JSONObject) outterObject;
+            if (source.contains(".")) {
+                String arg = source.substring(0, source.indexOf('.'));
+                deleteJsonProperty(tempJson.get(arg), source.substring(source.indexOf('.') + 1));
+                return;
+            }
+            tempJson.remove(source);
+        } else if (outterObject instanceof JSONArray) {
+            JSONArray tempArray = (JSONArray) outterObject;
+            for (int i = 0; i < tempArray.size(); i++) {
+                if (tempArray.get(i) instanceof JSONArray) {
+                    JSONArray tempJson = (JSONArray) tempArray.get(i);
+                    deleteJsonProperty(tempJson, source);
+                } else {
+                    JSONObject tempJson = (JSONObject) tempArray.get(i);
+                    if (source.contains(".")) {
+                        String arg = source.substring(0, source.indexOf('.'));
+                        if (tempJson.get(arg) == null)
+                            continue;
+                        deleteJsonProperty(tempJson.get(arg), source.substring(source.indexOf('.') + 1));
+                    } else {
+                        tempJson.remove(source);
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 递归实现字段保留功能
+     *
+     * @param outterObject
+     * @param source
+     */
+    private static void retainJsonProperty(Object outterObject, String source) {
+        if (outterObject == null || source == null || "".equals(source))
+            return;
+
+        if (outterObject instanceof JSONObject) {
+            JSONObject obj = ((JSONObject) outterObject);
+            if (source.contains(".")) {
+                String arg = source.substring(0, source.indexOf('.'));
+                retainJsonProperty(obj.get(arg), source.substring(source.indexOf('.') + 1));
+            } else
+                JsonSlimParser.keepJsonObjectByGivenKeys(obj, source);
+        } else if (outterObject instanceof JSONArray) {
+            JSONArray tempArray = (JSONArray) outterObject;
+            for (int i = 0; i < tempArray.size(); i++) {
+                if (tempArray.get(i) instanceof JSONObject) {
+                    JSONObject obj = tempArray.getJSONObject(i);
+                    if (source.contains(".")) {
+                        String arg = source.substring(0, source.indexOf('.'));
+                        retainJsonProperty(obj.get(arg), source.substring(source.indexOf('.') + 1));
+                    } else {
+                        JsonSlimParser.keepJsonObjectByGivenKeys(obj, source);
+                    }
+                } else {
+                    JSONArray jsonArray = (JSONArray) tempArray.get(i);
+                    retainJsonProperty(jsonArray, source);
+                }
+            }
+        }
+    }
+
+    public static void keepKeysByGivenKeySet(TreeSet<String> keySet, Object obj){
+        if(!(keySet instanceof TreeSet))
+            return;
+        for(String key:keySet){
+            retainJsonProperty(obj,key);
+        }
+    }
 }
--- src/main/java/com/ocft/gateway/common/parser/JsonSlimParser.java
+++ src/main/java/com/ocft/gateway/common/parser/JsonSlimParser.java
@@ -1,7 +1,13 @@
 package com.ocft.gateway.common.parser;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import java.util.*;
 
 /**
  * @author lijiaxing
@@ -10,7 +16,89 @@ import org.springframework.stereotype.Component;
  * @date 2019/11/23下午5:05
  * @Description: json剪裁解析器
  */
-@Slf4j
-@Component
 public class JsonSlimParser {
+    private static final String INNER_SPLIT_FLAG = ",";
+    /**
+     * @param jsonObj given manage jsonObejct
+     * @param keys    given keep keys
+     *                同一级的多个字段，用，隔开
+     */
+    public static void keepJsonObjectByGivenKeys(JSONObject jsonObj, String keys) {
+        String[] keyAttr = keys.split(INNER_SPLIT_FLAG);
+        Set<String> keyKeepSet = new HashSet<String>();
+        for (String s : keyAttr)
+            keyKeepSet.add(s);
+        Set<String> keyRemove = new HashSet<String>();
+        keyRemove.addAll(jsonObj.keySet());
+        keyRemove.removeAll(keyKeepSet);
+        for (String key : keyRemove)
+            jsonObj.remove(key);
+    }
+
+    /***
+     * pathStrt
+     * @param pathStr
+     * @return
+     */
+    public static Map<String, Object> pathStr2Map(String pathStr) {
+        Map<String, Object> pathMap = new HashMap<String, Object>();
+        String[] paths = pathStr.split(",");
+        for (String path : paths) {
+            String[] tmp = path.split("\\.");
+            if (tmp.length == 1) {
+                pathMap.put(tmp[0], null);
+            }
+            Map<String, Object> tmpMap = pathMap;
+            for (int i = 0; i < tmp.length; i++) {
+                if (tmpMap.get(tmp[i]) != null)
+                    tmpMap = (HashMap<String, Object>) tmpMap.get(tmp[i]);
+                else if (i < tmp.length) {
+                    tmpMap.put(tmp[i], new HashMap<String, Object>());
+                    tmpMap = (HashMap<String, Object>) tmpMap.get(tmp[i]);
+                } else
+                    tmpMap.put(tmp[i], null);
+            }
+        }
+        return pathMap;
+    }
+
+    /**
+     * 获取路径方法
+     *
+     * @param path
+     * @param map
+     */
+    public static void getKeepStylePath(String path, Map<String, Object> map, Set<String> resultSet) {
+        if (map == null || map.size() == 0) {
+            return;
+        }
+        if ("".equals(path)) {
+            resultSet.add(getKeySpiltBy(map));
+            path = "";
+        } else {
+            resultSet.add(path + "." + getKeySpiltBy(map));
+            path += ".";
+        }
+        for (String key : map.keySet()) {
+            getKeepStylePath(path + key, (Map<String, Object>) map.get(key), resultSet);
+        }
+    }
+
+    /**
+     * 获取key打印，用逗号隔开
+     *
+     * @param map
+     * @return
+     */
+    public static String getKeySpiltBy(Map<String, Object> map) {
+        if (map == null || map.size() == 0)
+            return "";
+        Set<String> set = map.keySet();
+        String resultStr = "";
+        for (String key : set) {
+            resultStr += key;
+            resultStr += ",";
+        }
+        return resultStr.substring(0, resultStr.length() - 1);
+    }
 }
