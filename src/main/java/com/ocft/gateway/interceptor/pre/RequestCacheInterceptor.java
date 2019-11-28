fd505306fa85ecf67a8c489a7db2d282b61a19f3
HarryJiangter <15756329950@163.com>
2019-11-28 10:27:42 +0800

    设置全局缓存开关

--- src/main/java/com/ocft/gateway/interceptor/pre/RequestCacheInterceptor.java
+++ src/main/java/com/ocft/gateway/interceptor/pre/RequestCacheInterceptor.java
@@ -5,6 +5,7 @@ import com.ocft.gateway.common.converter.GatewayContextConverter;
 import com.ocft.gateway.common.exceptions.GatewayException;
 import com.ocft.gateway.enums.ResponseEnum;
 import com.ocft.gateway.interceptor.AbstractGatewayInterceptor;
+import com.ocft.gateway.service.IGatewayCacheService;
 import com.ocft.gateway.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +25,15 @@ public class RequestCacheInterceptor extends AbstractGatewayInterceptor {
     @Autowired
     private RedisUtil redisUtil;
 
+    @Autowired
+    private IGatewayCacheService gatewayCacheService;
+
     @Override
     public void doInterceptor(GatewayContext context) {
         System.out.println("请求缓存拦截器被执行！");
         //检查缓存开关，如果缓存为关闭状态则退出方法执行handle方法
-        if (!context.getGatewayCache().getStatus()) return;
+        if (!gatewayCacheService.getGatewayCache("global").getStatus()
+                ||!context.getGatewayCache().getStatus()) return;
 
         //把请求body参数转换为“key1_vlaue1_key2_value2_...”的字符串
         String field = GatewayContextConverter.convertRedisHashField(context);
