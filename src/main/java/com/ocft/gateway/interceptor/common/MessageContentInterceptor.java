5b65abf701e1a240d53f7b340909b43638077059
singedli <634814660@qq.com>
2019-12-06 11:11:05 +0800

    报文内容转换拦截器

---
+++ src/main/java/com/ocft/gateway/interceptor/common/MessageContentInterceptor.java
@@ -0,0 +1,18 @@
+package com.ocft.gateway.interceptor.common;
+
+import com.ocft.gateway.common.context.GatewayContext;
+import com.ocft.gateway.interceptor.AbstractGatewayInterceptor;
+
+/**
+ * @author lijiaxing
+ * @Title: MessageContentInterceptor
+ * @ProjectName gateway
+ * @date 2019/12/6上午11:09
+ * @Description: 报文内容转换拦截器
+ */
+public class MessageContentInterceptor  extends AbstractGatewayInterceptor {
+    @Override
+    public void doInterceptor(GatewayContext context) {
+
+    }
+}
