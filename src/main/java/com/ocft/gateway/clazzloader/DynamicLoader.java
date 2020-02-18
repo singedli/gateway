54e8cb968ae6a1f862cd66c17967f71924bac469
bobby <bobby_white@163.com>
2020-02-18 09:25:44 +0800

    动态生成class对象，并调用该对象的方法  白涛

---
+++ src/main/java/com/ocft/gateway/clazzloader/DynamicLoader.java
@@ -0,0 +1,55 @@
+package com.ocft.gateway.clazzloader;
+
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author: Bobby
+ * @create: 2020-02-11 15:29
+ * @description:动态执行
+ **/
+public class DynamicLoader {
+
+    /**
+     * auto fill in the java-name with code, return null if cannot find the public class
+     *
+     * @param javaSrc source code string
+     * @return return the Map, the KEY means ClassName, the VALUE means bytecode.
+     */
+    public static Map<String, byte[]> compile(String javaSrc) {
+        Pattern pattern = Pattern.compile("public\\s+class\\s+(\\w+)");
+        Matcher matcher = pattern.matcher(javaSrc);
+        if (matcher.find()) {
+            return compile(matcher.group(1) + ".java", javaSrc);
+        }
+        return null;
+    }
+
+    public static Map<String, byte[]> compile(String javaName, String javaSrc) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager stdManager = compiler.getStandardFileManager(null, null, null);
+        try (MemoryJavaFileManager manager = new MemoryJavaFileManager(stdManager)) {
+            JavaFileObject javaFileObject = manager.makeStringSource(javaName, javaSrc);
+            JavaCompiler.CompilationTask task = compiler.getTask(null, manager, null, null, null, Arrays.asList(javaFileObject));
+            if (task.call())
+                return manager.getClassBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
+}
+
+
+
